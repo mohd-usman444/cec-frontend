d6439cd8f3f8cd3ead3c41d2e4503d99bb80d2c9
mohd-usman444 <mohdusman43863@gmail.com>
2026-05-15 23:12:49 +0530

    update ag frontend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: `${(import.meta.env.VITE_API_URL || 'http://localhost:5000')
+    .replace(/\/+$/, '')}/api`,
   withCredentials: true, // Important for sending/receiving HTTP-only cookies
   headers: {
     'Content-Type': 'application/json',
